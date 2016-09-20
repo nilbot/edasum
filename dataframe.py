95d21dcb576eaee01d79d0b38c9df3d8c7c5e034
Ersi Ni <ersi.ni@nilbot.net>
2016-09-20 16:43:47 +0100

    checkpoint 16-09-20

--- dataframe.py
+++ dataframe.py
@@ -2,5 +2,8 @@ import json
 import pandas as pd
 
 def as_dataframe(jl):
-	return pd.read_json('[%s]' % ','.join(jl.splitlines()))
-		
+    df = pd.read_json('[%s]' % ','.join(jl.splitlines()), convert_dates=True)
+    ## these 2 lines doesn't seem to work? #TODO
+    df['rating_date'] = pd.to_datetime(df['rating_date'], format='%Y-%m-%dT%H:%M:%S')
+    df.set_index('rating_date',inplace=True)
+    return df
